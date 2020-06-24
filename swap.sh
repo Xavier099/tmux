5fff960743093dd35c88af72056df32f088f8b78
Xavier099 <neutronzpx@gmail.com>
2020-06-25 01:39:55 +0700

    flex

---
+++ swap.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+# size of swapfile in megabytes
+echo "Enter Swap Size : "
+read -r SIZE
+
+# does the swap file already exist?
+grep -q "swapfile" /etc/fstab
+
+# if not then create it
+if [[ $? -ne 0 ]]; then
+   echo "swapfile not found. Adding swapfile."
+   sudo fallocate -l ${SIZE}M /swapfile
+   sudo chmod 600 /swapfile
+   sudo mkswap /swapfile
+   sudo swapon /swapfile
+   sudo cp /etc/fstab /etc/fstab.bak
+   echo "/swapfile none swap sw 0 0" >> /etc/fstab
+
+   echo "tuning swap"
+   sudo sysctl vm.swappiness=30
+   echo "vm.swappiness=30" >> /etc/sysctl.conf
+   sudo sysctl vm.vfs_cache_pressure=50
+   echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.conf
+else
+   echo "swapfile found. No changes made."
+fi
+
+# output results to terminal
+cat /proc/swaps
+cat /proc/meminfo | grep Swap
