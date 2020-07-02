b5f77eb3edc13d6285616ba1a56d30e63cbe88d6
Xavier099 <neutronzpx@gmail.com>
2020-07-02 22:21:45 +0700

    do

--- swap.sh
+++ swap.sh
@@ -13,8 +13,10 @@ grep -q "swapfile" /etc/fstab
 if [[ $? -ne 0 ]]; then
    echo "swapfile not found. Adding swapfile."
    if [[ -z $SIZE ]]; then
+      echo "swap size empty, add 1G as default swapfile"
       sudo fallocate -l 1G /swapfile
    else
+      echo "add ${SIZE}G as swapfile"
       sudo fallocate -l ${SIZE}G /swapfile
    fi
    sudo chmod 600 /swapfile
